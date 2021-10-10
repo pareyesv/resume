2d0a6107fb48e27ca0de861f328eb5e4ede79c92
Patricio Reyes <patricio.reyes@bsc.es>
2021-10-10 20:20:54 +0200

    chore: change the yaml files for the resume

--- constants.py
+++ constants.py
@@ -1,3 +1,8 @@
-MY_AUTHOR_NAME_LIST = ["Patricio Reyes", "P. Reyes", "P. Reyes Valenzuela", "Patricio Reyes Valenzuela"]
-CV_DATA_YAML = "cv_data.yml"
+MY_AUTHOR_NAME_LIST = [
+    "Patricio Reyes",
+    "P. Reyes",
+    "P. Reyes Valenzuela",
+    "Patricio Reyes Valenzuela",
+]
+CV_DATA_YAML = "cv_data-patricio_reyes.yml"
 PAPERS_YAML = "papers.yml"
